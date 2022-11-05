012dcccf5940fa5002e11a99cc0307a61971bed8
ALEXANDRE ALBERTO FIDALGO <alexandre.afidalgo@senacsp.edu.br>
2022-11-04 21:21:59 -0300

    Aula 04/11 parte 1

---
+++ cepapp/src/app/model/endereco.ts
@@ -0,0 +1,13 @@
+export class Endereco {
+    cep = '';
+    logradouro = '';
+    complemento = '';
+    bairro = '';
+    localidade = '';
+    uf = '';
+    ibge = '';
+    gia = '';
+    ddd = '';
+    siafi = ''
+  }
+  
